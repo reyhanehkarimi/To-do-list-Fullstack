65c1a330f6eb6641824a41bb09742baa9257514c
reyhanehkarimi <reyhanehkarimi9978@gmail.com>
2024-11-25 15:37:40 +0330

    backend-application

--- server/server.js
+++ server/server.js
@@ -9,15 +9,12 @@ require("dotenv").config();
 
 const app = express();
 
-// Middleware
 app.use(express.json());
 app.use(cors());
 
-// Routes
 app.use("/api/directories", directoryRoutes);
 app.use("/api/tasks", taskRoutes);
 
-// Database Connection and Server Start
 const PORT = process.env.PORT || 5000;
 connectDb();
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
